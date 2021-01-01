22235932352d44b5adc26365390d7d200c229b53
jenny87879 <68068885+jenny87879@users.noreply.github.com>
2021-01-01 17:19:59 +0900

    Input/output and four arithmetic operations
    backjoon Input and output and four arithmetic operations

---
+++ backjoon_2588.py
@@ -0,0 +1,10 @@
+num1 = int(input())
+num2 = int(input())
+num2_1 = int(str(num2)[2])
+num2_2 = int(str(num2)[1])
+num2_3 = int(str(num2)[0])
+
+print(num1*num2_1)
+print(num1*num2_2)
+print(num1*num2_3)
+print(num1*num2)
