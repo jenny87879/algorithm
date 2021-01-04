6aff753d279d01f77631cf7d048e4aaab1174606
JiYeon Park <68068885+jenny87879@users.noreply.github.com>
2021-01-05 00:48:15 +0900

    move fale

--- .vscode/baekjoon_10991.py
+++
@@ -1,9 +0,0 @@
-star = int(input())
-for i in range(1, star+1, 1):
-    for j in range(star-i):
-        print('@', end='')
-    # for k in range(1, star, 1):
-        print('*', end='')
-        for l in range(1, i-1, 1):
-            print('@*', end='')
-    print('')
